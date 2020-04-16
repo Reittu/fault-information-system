797e6b55463fb3e07553ab04a35e492a52584925
Reittu <Tony.Reittu@gmail.com>
2020-04-17 02:07:23 +0300

    Updates

---
+++ precache-manifest.73a5c0c8503840c56f46627a318d49d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55b2e9e5aeb416ab4965208ce2bc8cba",
+    "url": "/fault-information-system/index.html"
+  },
+  {
+    "revision": "447877d36a0215d5bb37",
+    "url": "/fault-information-system/static/css/main.8953568a.chunk.css"
+  },
+  {
+    "revision": "27c97265d0843f5935ab",
+    "url": "/fault-information-system/static/js/2.3c282380.chunk.js"
+  },
+  {
+    "revision": "cc7f4817303dc5ae24c42d02bfaefb45",
+    "url": "/fault-information-system/static/js/2.3c282380.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "447877d36a0215d5bb37",
+    "url": "/fault-information-system/static/js/main.e3ea1545.chunk.js"
+  },
+  {
+    "revision": "a8c7991e34872244f367",
+    "url": "/fault-information-system/static/js/runtime-main.059af64c.js"
+  }
+]);
